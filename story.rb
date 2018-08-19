56e9b36e0a0618b92825bbf566b43497b02b5548
kchoo2 <choo.keith@gmail.com>
2018-08-19 16:36:08 -0500

    final

--- story.rb
+++ story.rb
@@ -1,3 +1,4 @@
 puts "There once was a boy who lived in Chicago."
 puts "He made sure he always stayed alert so he wouldn't get robbed."
 puts "His efforts turned out to be futile and got robbed anyway."
+puts "He then moved out of Chicago."
